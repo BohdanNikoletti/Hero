d17929687efc7b0dcab13f0ef71d0ab361a66090
Bohdan <nikoletti@macpaw.com>
2020-12-14 15:15:59 +0200

    method return

--- Sources/Debug Plugin/HeroDebugPlugin.swift
+++ Sources/Debug Plugin/HeroDebugPlugin.swift
@@ -179,3 +179,8 @@ extension HeroDebugPlugin: HeroDebugViewDelegate {
 }
 
 #endif
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertToCAMediaTimingFunctionName(_ input: String) -> CAMediaTimingFunctionName {
+	return CAMediaTimingFunctionName(rawValue: input)
+}
